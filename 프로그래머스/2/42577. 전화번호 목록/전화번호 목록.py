ef2ce72bebf07b3cdce88c2ce30e6a6d187a4a97
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2024-03-27 23:55:01 +0900

    [level 2] Title: 전화번호 목록, Time: 76.37 ms, Memory: 28.1 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -2,7 +2,7 @@ def solution(phone_book):
     answer = True
     phone_book.sort() 
     
-    for i in range(0,len(phone_book)-1):
-        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+    for a, b in zip(phone_book, phone_book[1:]):
+        if b.startswith(a):
             return False
     return answer
