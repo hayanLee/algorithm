c6e994d67302a4cb5dfe13d37a0626f5a8383fde
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2024-04-22 13:49:10 +0900

    [level 1] Title: 음양 더하기, Time: 0.11 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/76501. 음양 더하기/음양 더하기.py
@@ -0,0 +1,8 @@
+def solution(absolutes, signs):
+    sum = 0
+    for absolute,sign in zip(absolutes, signs):
+        if sign:
+            sum+=absolute
+        else:
+            sum-=absolute
+    return sum
