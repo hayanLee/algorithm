798bf1bc691b055eabd440dd43058fb791e98aa4
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2024-04-25 09:58:32 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.06 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.py
@@ -0,0 +1,8 @@
+def solution(price, money, count):
+    # count를 횟수로 price가 배수로 증가
+    cost = 0
+    for i in range(1, count+1):
+        cost += price * i # 내야하는 돈
+    
+    if cost-money < 0: return 0
+    else: return cost-money
