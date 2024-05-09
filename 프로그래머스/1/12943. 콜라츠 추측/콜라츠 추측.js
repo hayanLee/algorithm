a0a5329193cff3fd1e899224a930734bf09018ff
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2024-05-09 09:46:41 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.11 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.js
@@ -0,0 +1,11 @@
+function solution(num) {
+    var answer = 0;
+    while(num > 1){
+        answer+=1
+        if(num % 2===0) num/=2
+        else{
+            num = num*3 + 1
+        }
+    }
+    return answer = answer < 500 ? answer : -1;
+}
