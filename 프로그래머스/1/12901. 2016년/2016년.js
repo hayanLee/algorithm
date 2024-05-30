f4b4e53a35aab186ee9b9c43e0305375602863b0
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2024-05-30 10:24:47 +0900

    [level 1] Title: 2016년, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    var answer = '';
+    const days= ['THU','FRI','SAT','SUN','MON','TUE','WED']
+    const months = [31,29,31,30,31,30,31,31,30,31,30,31]
+    
+    let sum =0
+    for(let i=0; i<a-1; i++) sum+=months[i]
+    
+    let idx = (sum + b)%7
+    return days[idx];
+}
