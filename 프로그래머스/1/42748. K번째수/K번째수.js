b9883f214a1aaf8f5d8e89fcdf1be5a981372957
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2024-07-05 09:36:35 +0900

    [level 1] Title: K번째수, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.js
+++ 프로그래머스/1/42748. K번째수/K번째수.js
@@ -1,8 +1,8 @@
 function solution(array, commands) {
     var answer = [];
-    for(let i=0; i<commands.length; i++){ 
-        let tmp = array.slice(commands[i][0]-1, commands[i][1]).sort((a,b) => a-b)
-        answer.push(tmp[commands[i][2]-1])
+    for(let i=0; i<commands.length; i++){
+        let t = commands[i]
+        answer.push(array.slice(t[0]-1, t[1]).sort((a,b) => a-b).at(t[2]-1))
     }
     return answer;
 }
