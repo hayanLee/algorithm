3dc53c54b33c0cad4201b9113ab1fa48cf635f2a
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2024-09-12 00:19:31 +0900

    [level 1] Title: 소수 찾기, Time: 29.93 ms, Memory: 45.6 MB -BaekjoonHub

--- 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.js
@@ -1,17 +1,23 @@
-function solution(n) {
-    var answer = 0;
+function findPrimes(n){
+    if(n<2) return 0; // 0,1은 미리 제외
     
     const arr = Array(n+1).fill(true) // 에라토스테네스의 체
-    for(let i = 2; i<Math.sqrt(n)+1; i++){ // 2~n의 제곱까지만 확인
+    arr[0] = arr[1] = false; // 0,1은 미리 제외
+    
+    const limit = Math.sqrt(n); // 제곱근 반복 계산 막기
+    for(let i = 2; i<=limit; i++){ // 2~n의 제곱까지만 확인
         if(arr[i]){ // 체크가 되어있으면 (= 소수이면) 
-            for(let j=2; i*j<=n; j++) // 해당 값의 배수를 모두 지움
-                arr[i * j] = false;
+            for(let j=i*i; j<=n; j+=i) arr[j] = false;// 해당 값의 배수를 모두 지움(i보다 작은 배수는 이미 지워졌으므로 i의 배수부터 시작)
         }
     }
     
-    for(let i=2; i<=n; i++){
-        if(arr[i] === true) answer++
-    }
-    
-    return answer;
+    return arr.reduce((primes, isPrime, idx) => {
+        if(isPrime) primes.push(idx)
+        return primes
+    }, [])
+}
+
+function solution(n) {
+    const primes = findPrimes(n);    
+    return primes.length;
 }
