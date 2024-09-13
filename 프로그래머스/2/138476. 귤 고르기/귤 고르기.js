949f876062059e3d09a2dace93423bd598f5ba5a
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2024-09-13 17:06:42 +0900

    [level 2] Title: 귤 고르기, Time: 74.77 ms, Memory: 62.9 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.js
@@ -1,18 +1,13 @@
 function solution(k, tangerine) {
     var answer = 0;
-    const map = new Map();
+    const map = tangerine.reduce((acc, t) => acc.set(t, (acc.get(t) || 0) + 1), new Map());
+    const sortedTangerines = [...map.entries()].sort((a, b) => b[1] - a[1]);
     
-    tangerine.forEach(t => {
-        if(map.has(t)) map.set(t, map.get(t)+1)
-        else map.set(t, 1)
-    })
-    const sortedMap = [...map].sort((a, b) => b[1] - a[1]);
-    
-    for(let i=0; i<sortedMap.length; i++){
-        if(k<=0) break;
-        k-=sortedMap[i][1]
-        answer+=1;
+    for (const [_, count] of sortedTangerines) {
+        if (k <= 0) break; 
+        k -= count;        
+        answer++;          
     }
-        
+    
     return answer;
 }
