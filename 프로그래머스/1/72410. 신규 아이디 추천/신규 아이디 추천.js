96820c24d52b5776a38c2507296b4f1240969d6b
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2024-09-21 00:15:47 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,24 +1,11 @@
-function solution(new_id) {
-    // 1단계
-    const first = new_id.toLowerCase()
+const solution = (new_id) => {
+    const id = new_id.toLowerCase()
+                    .replace(/[^a-z0-9\-_\.]/g, "")
+                    .replace(/\.{2,}/g, ".")
+                    .replace(/^\.|\.$/g, "")
+                    .padEnd(1, 'a')
+                    .slice(0,15)
+                    .replace(/\.+$/g, "")
     
-    // 2단계
-    const second = first.replace(/[^a-z0-9\-_\.]/g, "");
-   
-    // 3단계
-    const third = second.replace(/\.{2,}/g, ".");
-    
-    // 4단계
-    const fourth = third.replace(/^\.|\.$/g, "");
-    
-    // 5단계
-    const fifth = fourth.length === 0 ? "a" : fourth
-    
-    // 6단계
-    const sixth = fifth.length >= 16 ? fifth.slice(0, 15).replace(/\.+$/g, "") : fifth;
-    
-    // 7번째
-    const seventh = sixth.length <= 2 ? sixth.padEnd(3, sixth[sixth.length-1]) : sixth
-    
-    return seventh;
+    return id.padEnd(3, id[id.length-1])
 }
