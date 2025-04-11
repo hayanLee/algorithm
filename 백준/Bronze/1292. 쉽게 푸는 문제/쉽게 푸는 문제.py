5cc3d0f2975a50674d6f5b4e98ed2a0ff784e3d1
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2025-04-11 18:11:37 +0900

    [Bronze I] Title: 쉽게 푸는 문제, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
+++ 백준/Bronze/1292. 쉽게 푸는 문제/쉽게 푸는 문제.py
@@ -2,8 +2,10 @@
 #1부터 시작
 a,b = map(int, input().split())
 lts = []
-for i in range(1,b+1): 
-  for j in range(i): 
-    lts.append(i)
+
+i = 1 
+while len(lts) < b:
+  lts += [i]*i
+  i+=1
 
 print(sum(lts[a-1:b]))
