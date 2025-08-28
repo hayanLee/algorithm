4b2bf6f5cacb208810c24c978de4a6b00c506b2e
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2025-08-29 00:43:45 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.31 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,6 +1,10 @@
+# i번째 숫자부터 j번째 숫자까지 자르고 정렬 후 -> k번째 수
 def solution(array, commands):
     answer = []
-    for i,j,k in commands:
-        tmp = sorted(array[i-1:j])
-        answer.append(tmp[k-1])
+    
+    for T in range(len(commands)):
+        i,j,k = commands[T]
+        lts = sorted(array[i-1:j])
+        # print(lts)
+        answer.append(lts[k-1])
     return answer
