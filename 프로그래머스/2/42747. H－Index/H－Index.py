e9b32e7a5062d80e64af04f416ed06b7624e9583
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2025-08-29 18:55:23 +0900

    [level 2] Title: H-Index, Time: 0.24 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,8 +1,15 @@
+# (lts[i] >= h) >= h
+# h > 나머지 논문 
 def solution(citations):
     answer = 0
-    citations.sort(reverse=True) # 내림차순 정렬
+    n = len(citations)
+    lts = sorted(citations, reverse=True) # 인용횟수 내림차순 정렬 
     
-    for idx, value in enumerate(citations):
-        if idx >= value:
-            return idx
-    return len(citations)
+    for i in range(n):
+        if lts[i] >= i+1:
+            answer+=1
+        else: # 해당 값보다 작으면 나머지 값들은 모두 다 작으므로
+            break
+            
+    print(lts)
+    return answer
--- 프로그래머스/2/42747. H－Index/README.md
+++ 프로그래머스/2/42747. H－Index/README.md
@@ -1,10 +1,10 @@
 # [level 2] H-Index - 42747 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=javascript) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42747?language=python3) 
 
 ### 성능 요약
 
-메모리: 33.3 MB, 시간: 0.48 ms
+메모리: 9.26 MB, 시간: 0.24 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 09월 19일 22:34:54
+2025년 08월 29일 18:55:20
 
 ### 문제 설명
 
