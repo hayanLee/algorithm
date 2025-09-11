90300b88b13bd84107d98113296b2b8dcdbbaa9c
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2025-09-11 12:51:39 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.02 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.py
@@ -1,13 +1,11 @@
-# 지폐 큰쪽 절반 접기 < 90도 돌리기
+# 긴쪽을 절반으로 접기
+# max(지폐) < max(지갑) and min(지폐) < min(지갑) #종료 조건
 def solution(wallet, bill):
     answer = 0
-    n = len(wallet) # 2
-    
-    while min(bill) > min(wallet) or max(bill) > max(wallet):
-        if bill[0] > bill[1]: # 가로 > 세로
-            bill[0] = bill[0]//2
+    while max(bill) > max(wallet) or min(bill) > min(wallet):
+        if bill[0] > bill[1]:
+            bill[0] = bill[0] // 2
         else:
-            bill[1] = bill[1]//2
+            bill[1] = bill[1] // 2
         answer+=1
-        
     return answer
