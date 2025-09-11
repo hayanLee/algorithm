8e6479a7bd3154193cec70a6df0d877202a9851c
Hayan Lee <164024424+hayanLee@users.noreply.github.com>
2025-09-11 12:42:59 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 이웃한 칸, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
+++ 프로그래머스/1/250125. ［PCCE 기출문제］ 9번 ／ 이웃한 칸/［PCCE 기출문제］ 9번 ／ 이웃한 칸.py
@@ -1,14 +1,11 @@
 def solution(board, h, w):
-    answer = 0 
+    answer = 0
     n = len(board)
-    dh = [-1,0,1,0]
-    dw = [0,1,0,-1]
+    dx = [-1,0,1,0]
+    dy = [0,1,0,-1]
     
     for i in range(4):
-        nh = h + dh[i]
-        nw = w + dw[i]
-        if 0<=nh<n and 0<=nw<n:
-            if board[nh][nw] == board[h][w]:
-                answer+=1
+        if 0<=h+dx[i]<n and 0<=w+dy[i]<n and board[h+dx[i]][w+dy[i]] == board[h][w]:
+            answer+=1
     
     return answer
